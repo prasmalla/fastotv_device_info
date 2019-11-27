4ef84a6fef8c20ded119f3a7c87ce90ef254c1dd
Alexandr Topilski <atopilski@gmail.com>
2019-11-27 11:36:33 +0300

    Update FastotvDeviceInfoPlugin.m

--- ios/Classes/FastotvDeviceInfoPlugin.m
+++ ios/Classes/FastotvDeviceInfoPlugin.m
@@ -11,7 +11,7 @@
 
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
   if ([@"getTouch" isEqualToString:call.method]) {
-    result(true);
+    result(@(YES));
   } else {
     result(FlutterMethodNotImplemented);
   }
