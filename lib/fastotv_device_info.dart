892c115475321a6aa6a2b0e0722c4c980fd08c97
topilski <atopilski@gmail.com>
2020-09-25 08:54:10 +0300

    Style

--- lib/fastotv_device_info.dart
+++ lib/fastotv_device_info.dart
@@ -3,8 +3,7 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class FastotvDeviceInfo {
-  static const MethodChannel _channel =
-      const MethodChannel('fastotv_device_info');
+  static const MethodChannel _channel = const MethodChannel('fastotv_device_info');
 
   static Future<bool> get hasTouch async {
     final hasTouch = await _channel.invokeMethod('getTouch');
